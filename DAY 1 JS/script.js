97a48b9998f5dda8ee9fa4c96feedf6938601148
ANURAG SINGH <74447827+anuragsinghok@users.noreply.github.com>
2024-01-27 17:14:34 +0530

    Add files via upload

---
+++ DAY 1 JS/script.js
@@ -0,0 +1,17 @@
+alert("hello world")
+
+console.log("code is running...")
+
+var a = prompt("enter your number ")
+var isTrue = confirm("Are you sure to blast you conputer ")
+
+if(isTrue){
+    console.log("conputer is blasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+
+console.log("Your number is .."+a)
+
+document.title = ("Hey i am  good")
