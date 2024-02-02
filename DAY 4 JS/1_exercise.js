134de8bbd8c7cc38cb317b216c5005dba1533d90
ANURAG SINGH <74447827+anuragsinghok@users.noreply.github.com>
2024-02-02 23:50:10 +0530

    Update 1_exercise.js

--- DAY 4 JS/1_exercise.js
+++ DAY 4 JS/1_exercise.js
@@ -10,3 +10,33 @@
 // it perform wrong operation 10% of the time 
 // you can do it using random number gerator and 10 se niche hai to eroor ayega 
 // Math.random ( ) --> ka use kro aur dalo
+
+
+function faultycal(a,b,user) {
+
+    if(user == "+" && a<10 ){
+        console.log("the sum wro is "+ a-b)
+    }
+    else if(user == "*" && b>10 ){
+        console.log("the multi wro is "+ a+b)
+
+    }
+    else if(user == "-" && a>20 && a<10  ){
+        console.log("the minus wro is "+ a/b)
+
+    }
+    else if(user == "/" && b>20 && a<10  ){
+        console.log("the minus wro is "+ a**b)
+
+    }
+    else{
+        console.log(auserb)
+    }
+
+    
+}
+
+let a = prompt("enter the first number")
+let b = prompt("enter the second number")
+let user = prompt("select a function you want to perform ")
+faultycal(a,b,user)
